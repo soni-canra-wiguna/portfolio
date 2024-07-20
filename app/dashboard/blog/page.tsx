fb1de22b4491a95d30a9b65e28874316a03ae9e3
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-07-20 17:45:30 +0700

    feat: add create article button

--- app/dashboard/blog/page.tsx
+++ app/dashboard/blog/page.tsx
@@ -1,9 +1,19 @@
 import ListArticle from "@/app/dashboard/blog/_components/list-article"
+import { Button } from "@/components/ui/button"
+import { PlusCircle } from "lucide-react"
+import Link from "next/link"
 
 export default function DashboardBlog() {
   return (
     <div className="min-h-screeen mx-auto w-full max-w-7xl">
-      <h1 className="mt-14 text-xl">Blogs dashboard</h1>
+      <div className="mb-6 mt-10">
+        <h1 className="text-2xl font-medium">Dashobard Article</h1>
+        <Link href="/dashboard/blog/post-blog">
+          <Button>
+            <PlusCircle className="mr-2 size-4" /> Create Article
+          </Button>
+        </Link>
+      </div>
       <ListArticle />
     </div>
   )
